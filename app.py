e43b47354d634cf0c36636f7db7f198310da4920
Sindhu N <sindhuureddy03@gmail.com>
2025-01-10 14:25:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from tensorflow.keras.models import load_model
 from PIL import Image
 
 # Load the pre-trained model
-MODEL_PATH = "models/crop_classification_model.h5.py"
+MODEL_PATH = "models/crop_classification_model.h5"
 model = load_model(MODEL_PATH)
 
 # Define the categories
